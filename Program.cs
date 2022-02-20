eb15bb4049c49e0500b79c53a19d04a747475d56
Starkash <99684327+Starkash@users.noreply.github.com>
2022-02-21 00:19:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConvertString
+{
+    class Lower
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter any string:");
+            string s = Console.ReadLine();
+           
+
+            Console.WriteLine(s.ToLower());
+            
+        }
+    }
+}
